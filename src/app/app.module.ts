2a88da125f325fa6750c3b49d4bd7302c94d667a
Hector Sanchez <20186183@itla.edu.do>
2020-03-15 18:16:26 -0400

    Componente Home y Navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegacionComponent } from './Componentes/navegacion/navegacion.component';
+import { HomeComponent } from './Componentes/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacionComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
