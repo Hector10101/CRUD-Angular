8fbc70dbe034250216c57e340ee55df41e3aa563
Hector Sanchez <20186183@itla.edu.do>
2020-03-16 10:29:53 -0400

    Ruta addProveedor y elementos en la vista

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import {RouterModule, Routes} from '@angular/router';
 import { HomeComponent} from './Componentes/home/home.component';
 import { ProveedoresComponent} from './Componentes/Proveedores/proveedores/proveedores.component'
+import { AddProveedorComponent } from './Componentes/Proveedores/add-proveedor/add-proveedor.component';
 
 const app_routes: Routes =[
     { path: 'home', component: HomeComponent},
     { path: 'proveedores', component: ProveedoresComponent},
+    { path: 'addProveedores', component: AddProveedorComponent},
     { path:'**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
